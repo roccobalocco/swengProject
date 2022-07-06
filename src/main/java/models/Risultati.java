3d54535ad4607117b44127db8d189c507c916129
roccobalocco <72085385+roccobalocco@users.noreply.github.com>
2022-07-06 21:14:52 +0200

    Finished Risultati, but W/out TESTING!!!!!!!!!!

--- src/main/java/models/Risultati.java
+++ src/main/java/models/Risultati.java
@@ -1,15 +1,21 @@
 package models;
 
-import java.nio.file.Path;
-import java.util.Iterator;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
 
 public class Risultati {
 
     private boolean isClassica = true;
-    private Classica c;
-    private Referendum r;
-    private int si, no, bianca, totale;
+    private Classica c = null;
+    private Referendum r = null;
+    private int si = 0, no = 0, bianca = 0, totale = 0;
     private Map<Gruppo, Integer> votiGruppi;
     private Map<Persona, Integer> votiPersone;
 
@@ -28,49 +34,205 @@ public class Risultati {
     }
 
     public Risultati(Classica c, Map<Gruppo, Integer> vg){
-        this.r = r;
+        this.c = c;
         votiGruppi = vg;
         votiPersone = null;
     }
 
     public Risultati(Referendum r, int tot){
-        this.r = r;
+        votiGruppi = null; votiPersone = null; this.r = r;
         totale = tot;
         isClassica = false;
     }
 
     public Risultati(Referendum r){
-        this.r = r;
+        votiGruppi = null; votiPersone = null; this.r = r;
         isClassica = false;
     }
 
     private boolean hasWin(int voti){
-        return (totale/2) < voti;
+        return (int) (totale/2 - ((totale/2)%1.0)) < voti;
     }
 
     public boolean setRef(int si, int no, int bianca){
         if(isClassica)
             return false;
-        //TODO
+        this.si += si; this.no += no; this.bianca += bianca;
         return true;
     }
 
     public boolean setVoti(Gruppo g, Integer voti){
         if(!isClassica)
             return false;
-        //TODO
+        votiGruppi.put(g, voti + votiGruppi.get(g));
         return true;
     }
 
-    public boolean setVoti(Persona g, Integer voti){
+    public boolean setVoti(Persona p, Integer voti){
         if(!isClassica)
             return false;
-        //TODO
+        votiPersone.put(p, voti + votiPersone.get(p));
         return true;
     }
 
-    public void printRisultati(Path path){
-        //TODO
+    public String toString(){
+        if(c == null && r == null)
+            return "ERRORE NELLA CHIAMATA";
+        StringBuilder content = new StringBuilder();
+        if(isClassica) {
+            content.append(c.descrizione + "\n");
+            content.append("\n");
+            content.append("Scadenza: ").append(c.getScadenza().toString());
+            content.append("\n");
+            content.append("Si: ").append(si).append("\n");
+            content.append("No: ").append(no).append("\n");
+            content.append("Bianca: ").append(bianca).append("\n\n\n");
+            content.append(vinceRef());
+
+        }else{
+            content.append(r.descrizione + "\n");
+            content.append("\n");
+            content.append("Scadenza: ").append(r.getScadenza().toString());
+            content.append("\n");
+            content.append(gpString());
+            content.append(vinceCla());
+        }
+        return content.toString();
+    }
+    public boolean printRisultati(String path) throws FileNotFoundException, DocumentException {
+        if(c == null && r == null)
+            return false;
+        Document document = new Document();
+        if(isClassica) {
+            PdfWriter.getInstance(document, new FileOutputStream(path + "/" + c.descrizione.replaceAll("[ -/'\"]", "_") +
+                    c.getScadenza().toString().replaceAll("[ -/'\"]", "_") + ".pdf"));
+
+            document.open();
+            Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+            StringBuilder content = new StringBuilder(c.descrizione + "\n");
+            content.append("\n");
+            content.append("Scadenza: ").append(c.getScadenza().toString());
+            content.append("\n");
+            content.append("Si: ").append(si).append("\n");
+            content.append("No: ").append(no).append("\n");
+            content.append("Bianca: ").append(bianca).append("\n\n\n");
+            content.append(vinceRef());
+            Chunk chunk = new Chunk(content.toString(), font);
+
+            document.add(chunk);
+            document.close();
+        }else{
+            PdfWriter.getInstance(document, new FileOutputStream(path + "/" + r.descrizione.replaceAll("[ -/'\"]", "_") +
+                    r.getScadenza().toString().replaceAll("[ -/'\"]", "_") + ".pdf"));
+
+            document.open();
+            Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+            StringBuilder content = new StringBuilder(r.descrizione + "\n");
+            content.append("\n");
+            content.append("Scadenza: ").append(r.getScadenza().toString());
+            content.append("\n");
+            content.append(gpString());
+            content.append(vinceCla());
+            Chunk chunk = new Chunk(content.toString(), font);
+
+            document.add(chunk);
+            document.close();
+        }
+        return true;
+    }
+
+    private String gpString() {
+        StringBuilder s = new StringBuilder();
+        if(c.whichType() == 0 || c.whichType() == 2)
+            s.append("I voti espressi tramite ordine sono corrisposti in base al numero di candidati, se n é il numero di candidati, andranno n voti al primo, n-1 al secondo, e cosí via fino all'ultimo con un solo voto, a preferenza\n\n ");
+        s.append("Partiti/Gruppi:\n");
+        votiGruppi.forEach((g, i) -> {
+            s.append(g.toString()).append(" -- Voti:").append(i).append("\n");
+            if(c.whichType() == 2)
+                votiPersone.forEach((p, ii) -> {
+                    if (p.getGruppo() == g.getId())
+                        s.append("\t").append(p.toString()).append(" -- Preferenze secondo il calcolo del sistema: ").append(ii).append("\n");
+                });
+        });
+        return s.toString();
+    }
+
+    private String vinceCla() {
+        String s = "";
+        boolean pari = false;
+        int max = 0;
+        LinkedList<Gruppo> l = new LinkedList<>();
+        Gruppo w = null;
+        if(c.whichType() != 2){
+            for (Map.Entry<Gruppo, Integer> entry : votiGruppi.entrySet()) {
+                Gruppo g = entry.getKey();
+                Integer i = entry.getValue();
+                if (i > max) {
+                    l = new LinkedList<>();
+                    max = i;
+                    w = g;
+                    l.add(g);
+                    pari = false;
+                }else if(i == max){
+                    l.add(g);
+                    pari = true;
+                }
+            }
+
+            if(!c.isAssoluta() || hasWin(max))
+                if(pari)
+                    return "Non é possibile stabilire un vincitore a causa di una paritá tra: " + l.toString() + "\n\n";
+                else
+                    return "Il vincitore é: " + w + " -- Con " + max + " voti\n\n";
+            return "Non é stata raggiunta la maggioranza assoluta, non c'é nessun vincitore\n\n";
+        }else{
+            for (Map.Entry<Gruppo, Integer> entry : votiGruppi.entrySet()) {
+                Gruppo g = entry.getKey();
+                Integer i = entry.getValue();
+                if (i > max) {
+                    l = new LinkedList<>();
+                    max = i;
+                    w = g;
+                    l.add(g);
+                    pari = false;
+                }else if(i == max){
+                    l.add(g);
+                    pari = true;
+                }
+            }
+
+            if(!c.isAssoluta() || hasWin(max))
+                if(pari)
+                    return "Non é possibile stabilire un vincitore a causa di una paritá tra: " + l.toString() + "\n\n";
+                else {
+                    AtomicReference<String> ss = new AtomicReference<>("");
+                    Gruppo finalW = w;
+                    int finalMax = max;
+                    votiPersone.forEach((p, i) -> {
+                        if(p.getGruppo() == finalW.getId())
+                            ss.set("Il vincitore é: " + finalW + "\nPer il candidato: " + p + "\nCon " + finalMax + " voti\n\n");
+                    });
+                    return ss.toString();
+                }
+
+            return "Non é stata raggiunta la maggioranza assoluta, non c'é nessun vincitore\n\n";
+        }
+    }
+
+    private String vinceRef() {
+        if(r.hasQuorum()) {
+            if (hasWin(si))
+                return "Ha vinto il [ SI ] con " + si + " voti su " + (si + no + bianca) + " voti \n\n";
+            if (hasWin(no))
+                return "Ha vinto il [ NO ] con " + no + " voti su " + (si + no + bianca) + " voti \n\n";
+            return "Non é stato raggiunto il quorum\nSoli " + (si + no + bianca) + " su " + totale + "voti\n\n";
+        }else{
+            if (si > no)
+                return "Ha vinto il [ SI ] con " + si + " voti su " + (si + no + bianca) + " voti \n\n";
+            if (si < no)
+                return "Ha vinto il [ NO ] con " + no + " voti su " + (si + no + bianca) + " voti \n\n";
+            return "Situazione di paritá tra si [" + si + "] e no [" + no + "]\n\n";
+        }
     }
 
 }
